f43452ff4af1557fb186768ecb9b8048dd0bf5d0
denis <e1527687@student.tuwien.ac.at>
2022-04-04 19:36:15 +0200

    test implemented, repo link pasted

--- src/main/java/MCP.java
+++ src/main/java/MCP.java
@@ -5,6 +5,10 @@ public class MCP {
     /*
     controller class is always the one I make a singleton out of it
      */
+    /*
+    GITHUB REPO
+    https://github.com/denajaha/trafficlight2021Observer
+     */
     public static void main(String[] args) {
        // TrafficLightCtrl ctrl = new TrafficLightCtrl();
         TrafficLightCtrl ctrl = TrafficLightCtrl.getInstance();
--- src/test/java/TrafficLightCtrlTest.java
+++ src/test/java/TrafficLightCtrlTest.java
@@ -1,4 +1,16 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import trafficlight.ctrl.TrafficLightCtrl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TrafficLightCtrlTest {
 
-
+    @Test
+    @DisplayName("First test")
+    void testForInstance() {
+        TrafficLightCtrl actual = TrafficLightCtrl.getInstance();
+        TrafficLightCtrl expected = actual;
+        assertEquals(expected, actual);
+    }
 }
